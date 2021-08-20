1e8e4a550f641cd8582b70ed6d2f1eb06cc6dc2e
Juliana Sant <85078371+kajuxpri@users.noreply.github.com>
2021-08-20 00:25:17 -0300

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-function retornaLista (lista) {
-var listaClientes = [];
-for (var i = 0; i < lista.length; i++) {
-if (lista[i].idade>18 && lista[i].idade<26) {
-listaClientes.push(lista[i]);
-}
-}
-return listaClientes;
-}
