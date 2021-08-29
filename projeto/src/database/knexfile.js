3e1c76f8550b57728a880f63a7b265b84255c820
Juliana Santiago <jusantdp@gmail.com>
2021-08-29 00:17:59 -0300

    inclusão do banco de dados Mysql

---
+++ projeto/src/database/knexfile.js
@@ -0,0 +1,14 @@
+// Update with your config settings.
+
+module.exports = {
+  client: 'mysql',
+  connection: {
+    host : '127.0.0.1',
+    user : 'root',
+    password : 'Cajux2219!!',
+    database : 'pokemon'
+  },
+  debug: true  
+}
+
+
